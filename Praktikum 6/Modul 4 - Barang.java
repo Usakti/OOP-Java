204814651e2545c62abc41f4e4e247c98f2bb17d
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-11-14 07:35:33 +0700

    Create Modul 4 - Barang

---
+++ Praktikum 6/Modul 4 - Barang.java
@@ -0,0 +1,26 @@
+class Barang{
+	int id;
+	String kd,nm;
+
+	public Barang(int id, String kd, String nm)
+	{
+		this.id = id;
+		this.kd = kd;
+		this.nm = nm;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public String getKd()
+	{
+		return kd;
+	}
+
+	public String getNm()
+	{
+		return nm;
+	}
+}
